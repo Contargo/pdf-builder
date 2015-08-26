b2a9f5c641d3d74b7529c4f01170fdbb18756679
Olle Törnström <toernstroem@synyx.de>
2015-08-26 13:18:05 +0200

    Add strict parameter checks
    * The public API now requires non-null parameters, and is changed
      to use the more modern Path type (java.nio).
    
    * refs #12779

--- src/main/java/net/contargo/print/pdf/PDFReplace.java
+++ src/main/java/net/contargo/print/pdf/PDFReplace.java
@@ -1,5 +1,11 @@
 package net.contargo.print.pdf;
 
+import java.nio.file.Path;
+
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+
 /**
  * Main class for the PDF replacement tool, providing static utility methods to users.
  *
@@ -13,8 +19,25 @@ public final class PDFReplace {
         // Hidden
     }
 
-    public static void searchAndReplace() {
+    /**
+     * Searches and replaces texts in the given file based on the provided map of search-replace pairs.
+     *
+     * @param  path  path of the file to interpolate the given texts with
+     * @param  texts  map of search-replace pairs, to interpolate
+     *
+     * @return  the interpolated result as a byte array, never {@code null}
+     */
+    public static byte[] searchAndReplaceText(Path path, Map<String, String> texts) {
 
-        // TODO Auto-generated method stub
+        BiConsumer<String, Object> assertNotNull = (String n, Object o) -> {
+            if (o == null) {
+                throw new IllegalArgumentException(String.format("The %s must not be null", n));
+            }
+        };
+
+        assertNotNull.accept("path", path);
+        assertNotNull.accept("texts", texts);
+
+        return new byte[0];
     }
 }
--- src/test/java/net/contargo/print/pdf/PDFReplaceTest.java
+++ src/test/java/net/contargo/print/pdf/PDFReplaceTest.java
@@ -1,6 +1,14 @@
 package net.contargo.print.pdf;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import org.mockito.Mockito;
+
+import java.nio.file.Path;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -11,6 +19,25 @@ public class PDFReplaceTest {
     @Test
     public void ensureHasPublicStaticApiForSimpleSearchAndReplace() {
 
-        PDFReplace.searchAndReplace();
+        Path nonNullPath = Mockito.mock(Path.class);
+        Map<String, String> nonNullTextMap = new HashMap<>();
+
+        byte[] result = PDFReplace.searchAndReplaceText(nonNullPath, nonNullTextMap);
+
+        Assert.assertNotNull("Must not be null", result);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ensureSearchAndReplaceTextRequiresPathParameter() {
+
+        PDFReplace.searchAndReplaceText(null, new HashMap<>());
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ensureSearchAndReplaceTextRequiresTextsParameter() {
+
+        PDFReplace.searchAndReplaceText(Mockito.mock(Path.class), null);
     }
 }
